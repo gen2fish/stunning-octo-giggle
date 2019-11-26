8fb50a081b8938cb16e14aa64b99175b7efe3a10
Christopher Forkner <cforkner@hitachims.local>
2019-11-26 10:29:11 -0700

    Docker Compose with NGINX

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
 from flask import Blueprint
 from flask_restful import Api
+from resources.User import userAllAuth
 from resources.Movie import movieResource, movieAllResource
+from flask_httpauth import HTTPBasicAuth
 
+auth = HTTPBasicAuth()
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 # Route
-api.add_resource(movieAllResource, '/Movie')
-api.add_resource(movieResource, '/Movie/<int:mid>')
+api.add_resource(movieAllResource, '/movie')
+api.add_resource(movieResource, '/movie/<int:mid>')
+api.add_resource(userAllAuth, '/user')
+# api.add_resource(userAuth, '/user/<int:uid>')
